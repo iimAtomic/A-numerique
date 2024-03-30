948743385a5e17d19bf401b85ff3af24200a908f
iimAtomic <vegbahermerite@gmail.com>
2024-03-30 13:40:43 +0100

    yo

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
   return (
     <main>
       <Banner />
-       <CallToActionWithVideo /> 
+      <CallToActionWithVideo /> 
       <Mentor />
       <MultipleItems />
       <Newsletter />
--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-   
     reactStrictMode: true,
 	env: {
 		EMAIL_USERNAME:"vegbahermerite2@gmail.com",
